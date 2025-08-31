6bfef8e2082f526487d942135a39a0e79e953443
Rahul Roshan G <103802956+rahulroshanganesh@users.noreply.github.com>
2025-08-31 18:28:44 +0530

    Update App file
    Update app.py for playlist feature

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import *
 from datetime import datetime
 import validators
 from youtubeSummarizer import callModel
+from playList import get_playlist_thumbnail
 
 app = Flask(__name__)
 
@@ -16,11 +17,13 @@ def home():
             <h2>Title:- <input type="text" name="title"></h2>
             <h2>URL:- <input type="text" name="url"></h2>
             <input type="submit">
-            </form>
+        </form>
         <form methd="GET" action="/list">
             <input type="submit" value="See List" >
         </form>
-
+        <form methd="GET" action="/PlaylistLists">
+            <input type="submit" value="See PlayList Lists" >
+        </form>
         <div style="text-align:center;">
             <h1> ABOUT </h1>
             <h3>If Youtube Watch Later and Liked videos Playlist is filled and messed up use my app. I built this literally on midnight.</h3>
@@ -32,21 +35,32 @@ def go():
     url = request.form.get("url")
     title = request.form.get("title")
 
-    with open("db.txt", 'a') as file:
-        file.write(title + "-------" + url + "-------" + str(datetime.now()) + "\n")
-    return f"""
-            <h1>Added {title}</h1>
-            <button><h3><a href="/list"> See lists </a></h3></button>
-    """ 
+    if "playlist" in url:
+        print("inside go playlist")
+        with open("db_playlist.txt", 'a') as file:
+            file.write(title + "-------" + url + "-------" + str(datetime.now()) + "\n")
+        return f"""
+                <h1>Added {title} Playlist</h1>
+                <button><h3><a href="/PlaylistLists"> See lists </a></h3></button>
+        """
+    else:
+        print("inside go single video")
+        with open("db.txt", 'a') as file:
+            file.write(title + "-------" + url + "-------" + str(datetime.now()) + "\n")
+        return f"""
+                <h1>Added {title}</h1>
+                <button><h3><a href="/list"> See lists </a></h3></button>
+        """ 
 
-@app.route("/list", methods=["GET"])
-def list():
+@app.route("/PlaylistLists", methods=["GET"])
+def playlistLists():
     ListOfUrls = []
     template = ""
-    with open("db.txt", "r") as file:
+    with open("db_playlist.txt", "r") as file:
         ListOfUrls = file.readlines()
-
+    print(ListOfUrls)
     for item in ListOfUrls:
+        print(item)
         parts = item.strip().split("-------")
         if len(parts) != 3 or parts == "":
             continue  # skip invalid/old lines
@@ -54,7 +68,65 @@ def list():
         title, url, time = parts
 
         if "youtube" in url: 
-            domain,     id = url.split("=")
+            image_url,id = get_playlist_thumbnail(url)
+        else:
+            image_url = "https://support.vevo.com/hc/article_attachments/16164505898395"
+            id= ""
+        if not validators.url(url):
+            component = f"""
+                <h3 style="font-size:24px;">{url}</h3>
+            """
+        else:
+            component = f"""
+            <img src={image_url} width="120" style="margin-left:10px;"/>
+                <button>
+                    <a href={url} target="_blank">
+                        {title}
+                    </a>
+                </button> 
+            """
+        template += f"""
+                <li style="list-style:none; margin:5px 0;">
+                    {component}
+                    <span style="margin-left:10px; font-size:14px; color:gray;">
+                        at {time}
+                    </span>
+                    <form method="post" action="/remove"> 
+                        <input type="hidden" name="id" value="{url.split("=")[1]}">
+                        <input type="submit" value="DELETE" style="color:red; font-size:20px;">
+                    </form>
+                    
+                    
+                </li>
+            """
+
+    return f"""
+        <h1 style="text-align:center;">List of Useful Links</h1>
+        <ul style="padding:0;">
+            {template}
+        </ul>
+        <form method="GET" action="/">
+            <input type="submit" value="Add more">
+        </form>
+    """
+
+@app.route("/list", methods=["GET"])
+def list():
+    ListOfUrls = []
+    template = ""
+    with open("db.txt", "r") as file:
+        ListOfUrls = file.readlines()
+    # print(ListOfUrls)
+    for item in ListOfUrls:
+        # print(item)
+        parts = item.strip().split("-------")
+        if len(parts) != 3 or parts == "":
+            continue  # skip invalid/old lines
+
+        title, url, time = parts
+
+        if "youtube" in url: 
+            domain, id = url.split("=")
             videoId = f"https://img.youtube.com/vi/{id}/0.jpg"
         else:
             videoId = "https://support.vevo.com/hc/article_attachments/16164505898395"
@@ -99,29 +171,55 @@ def list():
         </form>
     """
 
-@app.route("/remove", methods=["POSt"])
+@app.route("/remove", methods=["POST"])
 def remove():
     id = request.form.get("id")
-    with open("db.txt", "r") as file:
-        lt = file.readlines()
+    print("what id length?  and id? ",len(id), id)
+    # playlist remove
+    if len(id) > 20:
+        with open("db_playlist.txt", "r") as file:
+            lt = file.readlines()
+        print("lt playlist: ",lt)
 
-    for item in lt:
-        if id in item:
-            dele = item
-            lt.remove(item)
+        for item in lt:
+            if id in item:
+                dele = item
+                print("dele: ",dele)
+                lt.remove(item)
+        print(lt)
+        with open("db_playlist.txt", "w") as file:
+            file.write("\n".join(lt))
 
-    with open("db.txt", "w") as file:
-        file.write("\n".join(lt))
+        return f"""
+        <h1>Deleted {dele.split("-------")[0]}</h1>
+        <p>You will be redirected to Home in 3 seconds...</p>
+        <script>
+            setTimeout(function(){{
+                window.location.href = "/";
+            }}, 3000);
+        </script>
+        """
+    else:
+        with open("db.txt", "r") as file:
+            lt = file.readlines()
 
-    return f"""
-    <h1>Deleted {dele.split("-------")[0]}</h1>
-    <p>You will be redirected to Home in 3 seconds...</p>
-    <script>
-        setTimeout(function(){{
-            window.location.href = "/";
-        }}, 3000);
-    </script>
-    """
+        for item in lt:
+            if id in item:
+                dele = item
+                lt.remove(item)
+
+        with open("db.txt", "w") as file:
+            file.write("\n".join(lt))
+
+        return f"""
+        <h1>Deleted {dele.split("-------")[0]}</h1>
+        <p>You will be redirected to Home in 3 seconds...</p>
+        <script>
+            setTimeout(function(){{
+                window.location.href = "/";
+            }}, 3000);
+        </script>
+        """
 
 @app.route("/summarizer", methods=["POST"])
 def summarizer():
